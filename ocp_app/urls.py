eaf774e0c4cad0f1c226ab285bba4ca7e3f32d8d
unknown <swapnilsingh6164@gmail.com>
2021-01-03 12:14:55 +0530

    getting user details from signup form

--- ocp_app/urls.py
+++ ocp_app/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index/', views.index, name='index'),
     path('signUpStud/', views.signUpStud, name='signUpStud'),
 ]
--- ocp_app/views.py
+++ ocp_app/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
-    return render(request, 'ocp_app/index.html')
-
-
 def signUpStud(request):
+    if request.method=="POST":
+        username = request.POST.get('username', '')
+        firstname = request.POST.get('firstname', '')
+        lastname = request.POST.get('lastname', '')
+        email = request.POST.get('email', '')
+        phone = request.POST.get('phone', '')
+        password = request.POST.get('password', '')
+        cnfpassword = request.POST.get('confpassword', '')
     return render(request, 'ocp_app/signUpStud.html')
