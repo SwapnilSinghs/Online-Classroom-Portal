27c90c73baed1254764d0fc46523e6ea52750a33
unknown <swapnilsingh6164@gmail.com>
2021-01-03 11:29:54 +0530

    html,css added to SignUp template

--- ocp_app/urls.py
+++ ocp_app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('index/', views.index, name='index'),
+    path('signUpStud/', views.signUpStud, name='signUpStud'),
 ]
--- ocp_app/views.py
+++ ocp_app/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'ocp_app/index.html')
+
+
+def signUpStud(request):
+    return render(request, 'ocp_app/signUpStud.html')
