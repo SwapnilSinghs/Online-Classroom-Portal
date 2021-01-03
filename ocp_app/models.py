0fc8c722da9d4ae15f35a64b825aeb16d26049ee
unknown <swapnilsingh6164@gmail.com>
2021-01-03 12:24:15 +0530

    created & migrated signUp model

--- ocp_app/models.py
+++ ocp_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
 # Create your models here.
+class SignUpStud(models.Model):
+    snum = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=15,default="")
+    firstname = models.CharField(max_length=20,default="")
+    lastname = models.CharField(max_length=20,default="")
+    email = models.CharField(max_length=100,default="")
+    phone = models.CharField(max_length=10,validators=[MinLengthValidator(10)],default="",help_text = "Enter 10 digit phone number")
+    password = models.CharField(max_length=20, default="")
