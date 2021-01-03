1ae689a893789322f6ede8f82f7ff75c6171ad46
unknown <swapnilsingh6164@gmail.com>
2021-01-03 12:51:06 +0530

    DB changed to mysql, created DB in localhost, made migrations of Student Signup Model

--- ocp/settings.py
+++ ocp/settings.py
@@ -74,11 +74,25 @@ WSGI_APPLICATION = 'ocp.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+'''
+DATABASES = {
+        'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'git_OCP',
+        'USER': 'root',
+        'PASSWORD': "",
+        'HOST': "",
+        'PORT': "",
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
--- ocp_app/views.py
+++ ocp_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
+from .models import SignUpStud
 
 # Create your views here.
 def signUpStud(request):
