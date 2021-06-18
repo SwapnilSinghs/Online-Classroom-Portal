ee8319608bd5ad7d3d3852d0652b412357978143
Sulabh Raj <pandeysulabhraj97@gmail.com>
2021-06-19 04:02:13 +0530

    admin update

--- admin_interface/urls.py
+++ admin_interface/urls.py
@@ -8,7 +8,23 @@ urlpatterns = [
     path('signuphandle/',views.signuphandle,name='signuphandle'),
     path('dashboard/',views.admin_dashboard,name='dashboard'),
     path('signOut/',views.signOut,name='signOut'),
-   
-   
-   
+    path('viewAnnouncement/',views.viewAnnouncements,name='viewAnnouncement'),
+    path('addAnnouncement/',views.addAnnouncements,name='addAnnouncement'),
+    path('add_announcement/',views.add_announcement,name='add_announcement'),
+    path('del_announce/',views.del_announce,name='del_announce'),
+    path('createDepartment/',views.createDepartment,name='createDepartment'),
+    path('create_Department/',views.create_Department,name='create_Department'),
+    path('viewDepartment/',views.viewDepartment,name='viewDepartment'),
+    path('del_department/',views.del_department,name='del_department'),
+    path('viewStudent/',views.viewStudent,name='viewStudent'),
+    path('search_student/',views.search_student,name='search_student'),
+    path('viewStudent/',views.viewStudent,name='viewStudent'),
+    path('search_student/',views.search_student,name='search_student'),
+    path('viewFaculty/',views.viewFaculty,name='viewFaculty'),
+    path('search_faculty/',views.search_student,name='search_faculty'),
+    path('del_student/',views.del_student,name='del_student'),
+    path('del_teacher/',views.del_teacher,name='del_teacher'),
 ]
+
+   
+   
--- admin_interface/views.py
+++ admin_interface/views.py
@@ -1,16 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.shortcuts import redirect
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect
 from django.contrib.auth.models import User, Group
-from ocp_app.models import Student, Department, Teacher, Courses,Announcement, Forum
+from django.urls import reverse
+
+from ocp_app.models import Student, Department, Teacher, Courses, Announcement, Forum , studyMaterial
+from django.contrib.auth.decorators import login_required
+from django.conf import settings
+from django.core.mail import send_mail
+import random
+import math
+from django.utils import timezone
+import datetime
+from django.core.files.storage import FileSystemStorage
+from django.http import HttpResponse, JsonResponse
+from django.http import FileResponse, Http404
+
 # Create your views here.
 def home(request):
     return render(request,'login.html')
 
 
-
+@login_required
 def admin_dashboard(request):
     id=request.user.username
     total_dep=Department.objects.all().count()
@@ -42,7 +55,7 @@ def signup(request):
 
 def signOut(request):
     logout(request)
-    return redirect(home)
+    return redirect(admin/home)
 
 
 def signuphandle(request):
@@ -56,17 +69,17 @@ def signuphandle(request):
 
         if not username.isalnum():
             messages.error(request, "Username must contain only letters or numbers.")
-            return redirect('/signup/')
+            return redirect('/admin/signup/')
         if firstname.isalpha() == False | lastname.isalpha() == False:
             messages.error(request, "Name must be alphabetical.")
-            return redirect('/signup/')
+            return redirect('/admin/signup/')
        
         if password!=confirm_pass:
             messages.error(request, "Password does not match.")
-            return redirect('/signup/')
+            return redirect('/admin/signup/')
         if len(password)<=8:
             messages.error(request, "Password should be greater than 8 characters.")
-            return redirect('/signup/')
+            return redirect('/admin/signup/')
 
         myuser = User.objects.create_user(username,email,password)
         myuser.first_name = firstname
@@ -74,3 +87,168 @@ def signuphandle(request):
         myuser.save()
 
         return render(request,'home.html')
+
+
+
+@login_required
+def viewAnnouncements(request):
+    id = request.user.username
+    announce = Announcement.objects.all()
+    params = {'user': id, 'announce': announce}
+    return render(request, 'viewAnnouncement.html', params)
+
+@login_required
+def addAnnouncements(request):
+    id = request.user.username
+    department = Department.objects.all()
+    depart=[]
+    for i in department:
+        depart.append(i.dept_id)
+    
+    params = { 'user': id, 'dept': depart}
+    return render(request, 'addAnnouncement.html', params)
+
+def add_announcement(request):
+    if request.method == "POST" and request.FILES['announce_file']:
+        file_type = request.POST.get('file_type', '')
+        imgfile = request.FILES['announce_file']
+        if file_type == 'img':
+            fs = FileSystemStorage()
+            imgfilename = fs.save(imgfile.name, imgfile)
+            imgurl = fs.url(imgfilename)
+        else:
+            imgurl = imgfile
+        name = request.POST.get('name', '')
+        detail = request.POST.get('detail', '')
+        file_type = request.POST.get('file_type', '')
+        dept = request.POST.get('dept', '')
+        depart = Department.objects.get(dept_id=dept)
+        announce = Announcement(announcement_name=name, detail=detail,
+                                department=depart, file_type=file_type, announcement_file=imgurl)
+        announce.save()
+    return redirect('../addAnnouncement')
+
+
+def del_announce(request):
+    if request.method == "GET":
+        aid=request.GET.get('sid','')
+        print(aid)
+        announce = Announcement.objects.get(announcement_id = aid)
+        announce.delete()
+        return JsonResponse({'status': 1})
+
+    else:
+        return JsonResponse({'status': 0})
+
+
+def createDepartment(request):
+    return render(request,'createDepartment.html')
+
+def create_Department(request):
+    if request.method == "POST":
+        dept_id=request.POST.get('dept_id','')
+        dept_name = request.POST.get('dept_name','')
+
+        if dept_id =="" or dept_name=="":
+            return redirect('../createDepartment')
+
+        depart=Department(dept_id=dept_id,dept_name=dept_name)
+
+        depart.save()
+
+    return redirect('../createDepartment')
+
+
+def viewDepartment(request):
+    id = request.user.username
+    depart=Department.objects.all()
+    print(depart)
+    params={'user':id,'department':depart}
+    return render(request,'viewDepartment.html',params)
+
+
+def del_department(request):
+    if request.method == "GET":
+        did=request.GET.get('sid','')
+        print(did)
+        depart = Department.objects.get(dept_id = did)
+        depart.delete()
+        return JsonResponse({'status': 1})
+
+    else:
+        return JsonResponse({'status': 0})
+
+
+def viewStudent(request):
+    id = request.user.username
+    params = {'user': id}
+    return render(request,'viewStudent.html',params)
+
+
+def search_student(request):
+    id = request.user.username
+
+    if request.method == 'GET':
+        sname=request.GET.get('sid','')
+        print(sname)
+        student=Student.objects.filter(username=sname).values()
+        student_data=list(student)
+        
+        if student :
+           
+            return JsonResponse({'status': 1 , 'student':student_data})
+
+        else:
+            return JsonResponse({'status': 0})
+
+    else:
+        return JsonResponse({'status': 0})
+
+def viewFaculty(request):
+    id = request.user.username
+    params = {'user': id}
+    return render(request,'viewFaculty.html',params)
+
+
+def search_faculty(request):
+    id = request.user.username
+
+    if request.method == 'GET':
+        sname=request.GET.get('sid','')
+        print(sname)
+        teacher=Teacher.objects.filter(username=sname).values()
+        teacher_data=list(teacher)
+        
+        if teacher :
+           
+            return JsonResponse({'status': 1 , 'teacher':teacher_data})
+
+        else:
+            return JsonResponse({'status': 0})
+
+    else:
+        return JsonResponse({'status': 0})
+
+
+
+def del_student(request):
+    if request.method == "GET":
+        sid=request.GET.get('sid','')
+        print(sid)
+        student = Student.objects.get(username = sid)
+        student.delete()
+        return JsonResponse({'status': 1})
+
+    else:
+        return JsonResponse({'status': 0})
+
+def del_teacher(request):
+    if request.method == "GET":
+        fid=request.GET.get('sid','')
+        print(fid)
+        teacher = Teacher.objects.get(username = fid)
+        teacher.delete()
+        return JsonResponse({'status': 1})
+
+    else:
+        return JsonResponse({'status': 0})
